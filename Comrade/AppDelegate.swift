4b9e5579f07dafbdfabd3f693b6b991b702d28b1
Savelii Kozlov <s.kozlov@kse.org.ua>
2025-12-11 21:21:48 +0200

    chore(core): configure SceneDelegate to launch TasksViewController
    - Set TasksViewController as root with UINavigationController
    - Removed default ViewController launch

--- Comrade/AppDelegate.swift
+++ Comrade/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    // MARK: - UISceneSession Lifecycle
+    // UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
